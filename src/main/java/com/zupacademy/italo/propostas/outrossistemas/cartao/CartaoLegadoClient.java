df5c69c77fc32f86a5ffb73d0b2c601b474aff84
Italo Alves <italo.alves@zup.com.br>
2021-09-12 07:36:07 -0300

    Configura OpenTracing

--- src/main/java/com/zupacademy/italo/propostas/outrossistemas/cartao/CartaoLegadoClient.java
+++ src/main/java/com/zupacademy/italo/propostas/outrossistemas/cartao/CartaoLegadoClient.java
@@ -2,25 +2,22 @@ package com.zupacademy.italo.propostas.outrossistemas.cartao;
 
 import com.zupacademy.italo.propostas.outrossistemas.analise.AnalisePropostaRequest;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
-@FeignClient(value = "cartao-resource", url = "${servicos.cartao.host}")
+@FeignClient(value = "cartaoLegadoClient", url = "${servicos.cartao.host}")
 public interface CartaoLegadoClient {
-    @RequestMapping(method = RequestMethod.POST, value = "/cartoes", produces = "application/json")
+    @GetMapping(value = "/cartoes")
     CartaoResponse consultaNumero(@RequestBody AnalisePropostaRequest request);
 
-    @RequestMapping(method = RequestMethod.GET, value = "/cartoes/{numeroCartao}", produces = "application/json")
+    @GetMapping(value = "/cartoes/{numeroCartao}")
     CartaoResponse consultaNumero(@PathVariable("numeroCartao") String numeroCartao);
 
-    @RequestMapping(method = RequestMethod.POST, value = "/cartoes/{numeroCartao}/bloqueios", produces = "application/json")
+    @PostMapping(value = "/cartoes/{numeroCartao}/bloqueios")
     RespostaApi notificaBloqueio(@PathVariable("numeroCartao") String numeroCartao, @RequestBody BloqueioLegadoRequest request);
 
-    @RequestMapping(method = RequestMethod.POST, value = "/cartoes/{numeroCartao}/avisos", produces = "application/json")
+    @PostMapping(value = "/cartoes/{numeroCartao}/avisos")
     RespostaApi notificaAvisoViagem(@PathVariable("numeroCartao") String numeroCartao, @RequestBody AvisoLegadoRequest request);
 
-    @RequestMapping(method = RequestMethod.POST, value = "/cartoes/{numeroCartao}/carteiras", produces = "application/json")
+    @PostMapping(value = "/cartoes/{numeroCartao}/carteiras")
     RespostaApi associaCarteira(@PathVariable("numeroCartao") String numeroCartao, @RequestBody CarteiraLegadoRequest carteiraLegadoRequest);
 }
